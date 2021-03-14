f164b798eb93574d03e02179b6e4820c96b60654
saraheulie <79435002+saraheulie@users.noreply.github.com>
2021-03-14 16:24:26 -0500

    second program

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,25 @@
+/*
+// Set your variables
+let cities = []
+let user_input = ""
+let doneWithCities = false
+let i = 0
+
+// First while loop
+while (doneWithCities === false) {
+  user_input = prompt('Type in a city. When you are done inputting cities, type: STOP')
+  if (user_input == "STOP"){
+    doneWithCities = true
+  } else {
+    cities.push(user_input)
+    doneWithCities = false
+  }
+}
+  
+// Second loop  
+while (i <= cities.length) {
+  console.log(cities[i].toLowerCase())
+  i++;
+}
+
+*/
---
+++ forms/forReverseStates/forReverseStates.js
@@ -0,0 +1,5 @@
+let states = ["Nebraska","Iowa","Minnesota","South Dakota","North Dakota","Kansas","Oklahoma","Texas"]
+
+for (i=states.length; i >= 0; i--) {
+  console.log(states[i])
+  }
